872bf4b769bd488ad36fb4376c91fe8c921d4fc6
FiacreKouablan <>
2023-11-22 21:33:50 -0500

    new commit

--- src/routes/sign-in/sign-in.component.js
+++ src/routes/sign-in/sign-in.component.js
@@ -1,18 +1,31 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import {useEffect } from 'react';
+import {getRedirectResult} from 'firebase/auth';
 
-
+import { auth, 
+         signInWithGooglePopup, 
+         createUserDocumentFromAuth, 
+         signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () =>{
+    useEffect(async () => {
+        const response = await getRedirectResult(auth);
+        console.log(response);
+    }, []);
+
     const logGoogleUser = async() => {
         const {user} = await signInWithGooglePopup();
        // console.log(response);
         const userDocRef = await createUserDocumentFromAuth(user);
     }
+
     return(
         <div>
             <h1>This is the sign-in component</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
             </button>
         </div>
     )
